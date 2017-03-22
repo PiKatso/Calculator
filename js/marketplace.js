0a364a33a51ec6ee273516b289470fe339d519fb
Aubrey Masten and Kat Gifford <kat@>
2017-03-22 15:04:20 -0700

    add order form

--- js/marketplace.js
+++ js/marketplace.js
@@ -1,3 +1,19 @@
 $(document).ready(function(){
+  $("form#order").submit(function(event) {
+    event.preventDefault();
+    var name = $("#name").val();
+    var address = $("#address").val();
+    var purchase = $("#purchase").val();
+
+    $("#rec-name").text(name);
+    $("#rec-address").text(address);
+    $("#rec-purchase").text(purchase);
+
+
+
+    $("#reciept").show();
+
+  });
+
 
 });

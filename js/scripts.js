d3fff37b99a4bcc9c1070b4721dcd24eb94a4d4b
Aubrey Masten and Kat Gifford <aubrey@>
2017-03-22 13:26:47 -0700

    add division, multiplication & subtraction forms

--- js/scripts.js
+++ js/scripts.js
@@ -18,12 +18,43 @@ var divide = function(number1, number2) {
 
 //User interface (front-end) logic:
 $(document).ready(function() {
+
   $("form#add").submit(function(event){
     event.preventDefault();
     var number1 = parseInt($("#add1").val());
     var number2 = parseInt($("#add2").val());
     var result = add(number1, number2);
 
-    $("#output").text(result);
+    $("#add-output").text(result);
   });
+
+  $("form#subtract").submit(function(event){
+    event.preventDefault();
+    var number1 = parseInt($("#sub1").val());
+    var number2 = parseInt($("#sub2").val());
+    var result = subtract(number1, number2);
+
+    $("#sub-output").text(result);
+  });
+
+  $("form#multiply").submit(function(event){
+    event.preventDefault();
+    var number1 = parseInt($("#mul1").val());
+    var number2 = parseInt($("#mul2").val());
+    var result = multiply(number1, number2);
+
+    $("#mul-output").text(result);
+  });
+
+  $("form#divide").submit(function(event){
+    event.preventDefault();
+    var number1 = parseInt($("#div1").val());
+    var number2 = parseInt($("#div2").val());
+    var result = divide(number1, number2);
+
+    $("#div-output").text(result);
+
+  });
+
+
 });
